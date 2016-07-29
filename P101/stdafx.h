a1631d48171a8939fa107d87a09a6e173d5586be
LBViktor <lebrun.victor@wanadoo.fr>
2016-07-29 09:38:32 +0200

    Lua bug correction

--- P101/stdafx.h
+++ P101/stdafx.h
@@ -9,13 +9,14 @@
 
 #include <stdio.h>
 #include <tchar.h>
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+extern "C" {
+#include "lua.hpp"
+}
 #include "lunar.h"
 
 #define GLEW_STATIC
 #pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "lua53.lib")
 
 #define method(class, name) {#name, &class::name}
 // TODO: reference additional headers your program requires here
